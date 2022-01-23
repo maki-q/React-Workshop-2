e17b6f1395d071dd86b35c27930f27bb2b66ec8a
Quinton Maki <maki-q@Quintons-MacBook-Pro.local>
2022-01-22 16:59:53 -0800

    Initial project setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+/*
+   USER STORY
+
+  As a user, I want to see all students displayed on a chart.
+  There should be check boxes next to each students name.
+  If the student is unchecked, their background should appear red.
+  If the student is checked, their background should appear green.
+
+  There should be an "add notes" button for each student, and clicking that button opens up a text field.
+  There should be a save button that gets rid of the box, and adds the text to a notes area.
+  If there is a note, there should be an edit note button instead of an add note button.
+
+  Green: #0AF592
+  Red: #FF2900
+
+*/
